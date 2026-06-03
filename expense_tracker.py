91662b776441b30f6663b7ac9c6d8716e4558002
Shashank Kumar <shashank.cm.kumar@gmail.com>
2026-06-03 10:19:44 +0530

    Added exit condition with a diff logic

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,24 +1,28 @@
-money_amount = int(input("Money Used: "))
-money_reason = (input("Money Used For: "))
-money_location = (input("Money Used At: "))
+while True:
+    money_amount = (input("Money Used: "))
+    if money_amount == "exit":
+        break
+    money_amount = int(money_amount)
+    money_reason = (input("Money Used For: "))
+    money_location = (input("Money Used At: "))
 
-list1 = f"{money_amount} | {money_reason} | {money_location}\n"
+    list1 = f"{money_amount} | {money_reason} | {money_location}\n"
 
-with open("expenses.txt", "a") as file:
-    file.write(list1)
+    with open("expenses.txt", "a") as file:
+        file.write(list1)
 
-category_spending = {}
-total_spending =  0
-with open("expenses.txt", "r") as file:
-    for items in file:
-        var = items.split(" | ")
-        value = float(var[0])
-        total_spending += value
+    category_spending = {}
+    total_spending =  0
+    with open("expenses.txt", "r") as file:
+        for items in file:
+            var = items.split(" | ")
+            value = float(var[0])
+            total_spending += value
 
-        category = var[1]
-        if category in category_spending:
-            category_spending[category] += value
-        else:
-            category_spending[category] = value
+            category = var[1]
+            if category in category_spending:
+                category_spending[category] += value
+            else:
+                category_spending[category] = value
 
-    print(f"Your Total expenditure is {total_spending}")
+        print(f"Your Total expenditure is {total_spending}")
