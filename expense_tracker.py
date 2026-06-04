56609aed1bbb9ae3f962df2eae4c47682b4dbee5
Shashank Kumar <shashank.cm.kumar@gmail.com>
2026-06-04 09:43:47 +0530

    Intialized display_summary function

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,16 +1,6 @@
-while True:
-    money_amount = (input("Money Used: "))
-    if money_amount == "exit":
-        break
-    money_amount = int(money_amount)
-    money_reason = (input("Money Used For: "))
-    money_location = (input("Money Used At: "))
+import keyboard
 
-    list1 = f"{money_amount} | {money_reason} | {money_location}\n"
-
-    with open("expenses.txt", "a") as file:
-        file.write(list1)
-
+def display_summary():
     category_spending = {}
     total_spending =  0
     with open("expenses.txt", "r") as file:
@@ -25,4 +15,26 @@ while True:
             else:
                 category_spending[category] = value
 
-        print(f"Your Total expenditure is {total_spending}")
+    print(f"\n--- [ LIVE BALANCE: {total_spending} ] ---")
+    for cat, amt in category_spending.items():
+        print(f" * {cat}: {amt}")
+    print("------------------------------------------\n")
+
+
+keyboard.add_hotkey('F2', display_summary)
+
+print("LEDGER RUNNING IN SECURE BACKGROUND MODE...")
+print("-> Press [F2] globally to display summaries.\n")
+
+while True:
+    money_amount = (input("Money Used: "))
+    if money_amount == "exit":
+        break
+    money_amount = int(money_amount)
+    money_reason = (input("Money Used For: "))
+    money_location = (input("Money Used At: "))
+
+    list1 = f"{money_amount} | {money_reason} | {money_location}\n"
+
+    with open("expenses.txt", "a") as file:
+        file.write(list1)
